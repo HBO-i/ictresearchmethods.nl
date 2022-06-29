1375b8a34f895e326bb582cd917b1ffb12c70297
Jochem Vogel <voge0031@hz.nl>
2022-06-29 18:34:59 +0200

    Add about page and remove (unused) saved methods route in menu

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -1,24 +1,91 @@
 <h1>About</h1>
+<p>
+	As an ICT student or professional, you need to solve all kind of ICT challenges. Answering the
+	questions and tackling the problems or opportunities of your ICT project requires research and
+	often a combination of various ICT research methods. The toolkit on this website offers you a set
+	of possible research methods and a framework to select the appropriate (combination of) methods.
+</p>
+
+<!--
+<p>
+	But to be honest: a wiki is not fun to use and who buys a pack, lol. Because of that reason this
+	app is created. It's a fun an intuitive way of getting information about the research methods.
+</p> -->
+
+<h2>Disclaimer</h2>
 
 <p>
-	Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro obcaecati odio, doloribus impedit
-	reiciendis reprehenderit enim quaerat asperiores magnam beatae? Aperiam ea exercitationem autem
-	quaerat, aliquam nulla corrupti. Veritatis enim rem modi accusamus quam quod quibusdam, soluta
-	alias delectus culpa similique nesciunt amet maiores, quidem optio, id sit! Magnam error eligendi
-	et aliquam. Provident ducimus laboriosam obcaecati quod facilis cumque maiores accusantium. Quo
-	quidem doloremque veritatis laudantium quos magnam, facilis cum omnis quibusdam accusantium
-	corrupti dolores nihil explicabo iste eius nostrum tempora? Dignissimos et, asperiores, quod
-	temporibus sunt dolorem dolore cum quasi, incidunt consequuntur consequatur modi at totam
-	repudiandae ab distinctio doloribus perspiciatis tenetur velit quae obcaecati fugiat blanditiis!
-	Eligendi quibusdam saepe optio sunt amet incidunt doloremque ut non accusamus mollitia quo
-	reiciendis iste repellat, aperiam, ex odit dolores consectetur itaque accusantium cumque cum.
-	Culpa alias vitae reprehenderit, dolores quibusdam nemo eveniet facilis et totam? Tenetur rem
-	temporibus veritatis at.
+	The creator of this app does not own any of this content. All the ICT Research Methods are
+	available via the official <a href="https://ictresearchmethods.nl/Main_Page" target="_blank"
+		>wiki</a
+	>
+	or you can be found in the
+	<a href="https://www.studystore.nl/p/9990002067426/ict-methodspack" target="_blank"
+		>ICT Methodspack</a
+	>.
+</p>
+
+<h2>Licensing</h2>
+
+<p>
+	All of its contents and the physical Methods pack have been realised as, and are maintained by
+	<a href="https://www.hbo-i.nl/" target="_blank">HBO-i </a> projects.
+</p>
+<p>The Method cards (except the domain specific cards) were written by:</p>
+
+<ul>
+	<li>Wilco Bonestroo</li>
+	<li>Marcel Meesters</li>
+	<li>Ralph Niels</li>
+	<li>Jan Dirk Schagen</li>
+	<li>Koen van Turnhout</li>
+</ul>
+<p>All Method cards were illustrated by Laura Henneke</p>
+
+<h2>About this app</h2>
+
+<p>
+	As mentioned before: all the content is already available, so why this app? Well.. A wiki is not
+	fun to use and who buys a pack, lol.
 </p>
 
 <p>
-	Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur voluptas commodi earum
-	maxime qui similique inventore minima error quos vel est odit hic quis, ab cupiditate voluptatibus
-	accusantium expedita illum! Obcaecati explicabo tenetur facere autem est soluta dolores, quaerat
-	possimus!
+	With this app I hope I can encourage students to use the ICT Research Methods in a fun way. You
+	can easily search for methods, save your favorite methods to access them later even faster, use
+	the app as PWA on your phone and even view your saved methods offline.
 </p>
+<p>
+	Oh and btw, there is a <kbd>CMD</kbd>
+	+ <kbd>K</kbd> search and soon a dark mode. I mean: what else do you need.
+</p>
+
+<h2>Future</h2>
+
+<p>This app is still in development, so you might see some changes in the near future.</p>
+
+<h2>Contributing</h2>
+
+<p>
+	I <a href="https://github.com/jochemvogel/ictmethods.nl" target="_blank">open sourced</a> the project,
+	so other people can implement their own improvements. Enjoy! ✌️
+</p>
+
+<style lang="scss">
+	h1,
+	h2 {
+		margin-bottom: 0;
+	}
+
+	p {
+		margin-top: 0.5em;
+
+		@include desktop {
+			font-size: 1em;
+		}
+	}
+
+	a {
+		color: var(--color-black);
+		font-weight: bold;
+	}
+</style>
