d15744830ff1a7ef70b6920579081bbd84274fff
Jochem Vogel <hello@jochemvogel.com>
2022-08-31 16:30:45 +0200

    Remove redundant lines of code in service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -5,25 +5,28 @@ const staticCache = `staticCache-v${version}`;
 
 const cachedFiles = files.filter((file) => !file.startsWith('/img'));
 
-const prerenderedStaticURLs = prerendered; // You don't want this -> Every URL is cached (page + json endpoint) > 7.5MB
+const prerenderedStaticURLs = prerendered;
 
-const returnSSRpage = (path: string) => caches.open('ssrCache').then((cache) => cache.match(path));
+const PLACEHOLDER_IMAGE_URL = '/fallback.png';
+const OFFLINE_PAGE = '/offline';
+const PRERENDERED_URLS = 'prerenderedURLs';
+
+const returnStaticPage = (path: string) =>
+	caches.open(PRERENDERED_URLS).then((cache) => cache.match(path));
 
 // Caches the svelte app (not the data)
 self.addEventListener('install', (event) => {
 	event.waitUntil(
 		Promise.all([
-			caches.open('prerenderedURLs').then((cache) => cache.addAll(prerenderedStaticURLs)),
+			caches.open(PRERENDERED_URLS).then((cache) => cache.addAll(prerenderedStaticURLs)),
 			caches.open(applicationCache).then((cache) => cache.addAll(build)),
 			caches.open(staticCache).then((cache) => cache.addAll(cachedFiles))
-		])
-			.then(self.skipWaiting())
-			.then(() => console.log('installed'))
+		]).then(self.skipWaiting())
 	);
 });
 
 // Removes old caches
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', (event: Event) => {
 	event.waitUntil(
 		clients.claim(),
 		caches
@@ -32,60 +35,32 @@ self.addEventListener('activate', (event) => {
 				return Promise.all(
 					keys
 						.filter(
-							(key) =>
-								key !== applicationCache &&
-								key !== staticCache &&
-								key !== 'methodCache' &&
-								key !== 'prerenderedURLs'
+							(key) => key !== applicationCache && key !== staticCache && key !== PRERENDERED_URLS
 						)
 						.map((key) => caches.delete(key))
 				);
 			})
 			.then(self.skipWaiting())
-			.then(() => console.log('activated'))
 	);
 });
 
+function isImage(fetchRequest: Request) {
+	return fetchRequest.method === 'GET' && fetchRequest.destination === 'image';
+}
+
 // Fire when any resource is fetched
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', (event: Event) => {
 	const request = event.request;
-	const requestURL = new URL(request.url);
 
-	// if (/(methods\.json)/.test(requestURL.pathname)) { // When url is /methods.json
-	// 	console.log(requestURL.pathname);
+	if (isImage(request)) {
+		// Get broken image placeholder from cache (seems not to work, yet)
+		return caches.match(PLACEHOLDER_IMAGE_URL);
+	}
 
-	// 	const returnOfflineMethods = () => {
-	// 		return fetch(event.request).catch(() => {
-	// 			return caches
-	// 				.open('methodsCache')
-	// 				.then((cache) => {
-	// 					return cache.keys().then((cacheKeys) => {
-	// 						return Promise.all(cacheKeys.map((cacheKey) => cache.match(cacheKey)));
-	// 					});
-	// 				})
-	// 				.then((cachesResponses) => {
-	// 					return Promise.all(cachesResponses.map((response) => response.json()));
-	// 				})
-	// 				.then((methods) => {
-	// 					const response = new Response(JSON.stringify(methods), {
-	// 						statusText: 'offline'
-	// 					});
-	// 					return response;
-	// 				});
-	// 		});
-	// 	};
-	// 	event.respondWith(returnOfflineMethods());
-	// } else
-
-	if (
-		/(\/methods\/)(\w+-?)*/.test(requestURL.pathname) &&
-		!/(.css)|(.js)$/.test(requestURL.pathname)
-	) {
-		const findOfflineMethod = () =>
-			caches
-				.match(request)
-				.then((response) => (response ? response : fetch(request)))
-				.catch(() => returnSSRpage('/offline'));
-		event.respondWith(findOfflineMethod());
-	} else event.respondWith(caches.match(request).then((cacheRes) => cacheRes || fetch(request)));
+	event.respondWith(
+		caches
+			.match(request)
+			.then((cacheRes) => cacheRes || fetch(request))
+			.catch(() => returnStaticPage(OFFLINE_PAGE))
+	);
 });
