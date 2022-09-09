675f373a9f630191603d2bde8334a999a0473be7
Jochem Vogel <hello@jochemvogel.com>
2022-09-09 21:52:27 +0200

    Update offline page

--- src/routes/offline/+page.svelte
+++ src/routes/offline/+page.svelte
@@ -1,7 +1,11 @@
+<svelte:head>
+	<title>Offline — ICT Research Methods</title>
+</svelte:head>
+
 <header>
 	<h1>Offline</h1>
 </header>
 <main>
-	<p>General offline</p>
-	<a href="/">&larr; Go back home</a>
+	<p>Oopsie, you're offline and the page you're trying to reach is not offline available. :'(</p>
+	<p>We recommend you to go back to the <a href="/">home page.</a></p>
 </main>
