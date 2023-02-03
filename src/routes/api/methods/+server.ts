5dbffaf64108434ea2a16943945189fd3a274f68
Jochem Vogel <hello@jochemvogel.com>
2023-02-03 21:34:44 +0100

    Add md files
    Restruturing of the api endpoints and content

---
+++ src/routes/api/methods/+server.ts
@@ -0,0 +1,14 @@
+import type { RequestHandler } from './$types';
+export const GET: RequestHandler = async () => {
+	let methods = await Promise.all(
+		Object.entries(import.meta.glob('$lib/content/**/*.md')).map(async ([path, page]) => {
+			const { metadata } = await page();
+			const slug = path.split('/').pop().split('.').shift();
+			return { ...metadata, slug };
+		})
+	);
+
+	return new Response(JSON.stringify(methods), {
+		status: 200
+	});
+};
