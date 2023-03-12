b263fe1532c5db43ab815a6fab9da94d7605c776
Jochem Vogel <hello@jochemvogel.com>
2023-03-12 13:13:56 +0100

    Update about page and add content to machine learning page

--- src/routes/machine-learning/+page.svelte
+++ src/routes/machine-learning/+page.svelte
@@ -1,1 +1,31 @@
 <h1>Machine Learning</h1>
+
+<p>
+	Machine Learning (ML) software is a software system with one or more components that learn from
+	data. This entails engineering a pipeline for the collection and pre-processing of data, the
+	training of an ML model, the deployment of the trained model to perform inference and the software
+	engineering of the encompassing software system that sends new input data to the model to get
+	answers.
+</p>
+
+<p>
+	<a
+		href="https://fontysblogt.nl/software-engineering-for-machine-learning-applications/"
+		target="_blank"
+		>This post on ML projects
+	</a>explains why ML projects are different from traditional rule-based software engineering and
+	identifies eight challenges for engineering machine learning applications:
+</p>
+
+<ol>
+	<li>Data requirements engineering including data visualizations</li>
+	<li>ML components are more difficult to handle as distinct modules</li>
+	<li>Design of the ML component through algorithm selection and tuning</li>
+	<li>Break up the ML development in increments</li>
+	<li>Data and model management for the current and future projects</li>
+	<li>Find ML models that can be reused for your application</li>
+	<li>Validation of ML applications in absence of a specification to test against</li>
+	<li>Explainability of ML models is needed for debugging</li>
+</ol>
+
+<p>tbc.</p>
