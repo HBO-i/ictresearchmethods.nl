ba2a1f150d9af91027b853ab234faf1d1090907e
Jochem Vogel <hello@jochemvogel.com>
2023-10-03 13:42:56 +0200

    Remove coming soon and add small updates

--- src/lib/components/SiteDescription.svelte
+++ src/lib/components/SiteDescription.svelte
@@ -1,1 +1,5 @@
-<p>As an ICT student or professional, you need to solve all kind of ICT challenges. Answering the questions and tackling the problems or opportunities of your ICT project requires research and often a combination of various ICT research methods. The toolkit on this website offers you a set of possible research methods and a framework to select the appropriate (combination of) methods.</p>
+<!-- <p>As an ICT student or professional, you need to solve all kind of ICT challenges. Answering the questions and tackling the problems or opportunities of your ICT project requires research and often a combination of various ICT research methods. The toolkit on this website offers you a set of possible research methods and a framework to select the appropriate (combination of) methods.</p> -->
+
+<p>Welcome to the new webiste, release in October 2023. Please let us know if you have any feedback by <a href="https://github.com/HBO-i/ictresearchmethods.nl/issues/new/choose" target="_blank">creating an issue on GitHub</a>.</p>
+
+<p>You can still access the old website, by visiting this <a href="https://oud.ictresearchmethods.nl/" target="_blank">url</a>.</p>
