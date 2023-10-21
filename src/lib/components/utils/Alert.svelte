05dcabc3c2f61d6b24511dd673304a68d48fa7ba
Coen Warries <coen.warries@student.hu.nl>
2023-10-21 15:07:09 +0200

    Add icon switch to Alert component

--- src/lib/components/utils/Alert.svelte
+++ src/lib/components/utils/Alert.svelte
@@ -1,14 +1,32 @@
 <script lang="ts">
 	import InfoIcon from "$lib/assets/icons/utils/InfoIcon.svelte";
+	import AlertIcon from "$lib/assets/icons/utils/AlertIcon.svelte";
+	import AlertTriangleIcon from "$lib/assets/icons/utils/AlertTriangleIcon.svelte";
+	import CheckCircleIcon from "$lib/assets/icons/utils/CheckCircleIcon.svelte";
 
     export let type: 'error' | 'warn' | 'info' | 'success' | 'primary' = 'info';
     export let title: string = type[0].toUpperCase() + type.slice(1).toLocaleLowerCase();
+
+		function getIcon() {
+			switch (type) {
+				case "error":
+					return AlertIcon
+				case "warn":
+					return AlertTriangleIcon
+				case "info":
+					return InfoIcon
+				case "success":
+					return CheckCircleIcon
+				default:
+					return InfoIcon
+			}
+		}
 </script>
 
 <div class="alert-wrapper">
     <div class="alert alert-{type}">
         <div class="alert-icon">
-            <InfoIcon />
+					<svelte:component this="{getIcon()}"/>
         </div>
         <div class="alert-content">
             <div class="alert-title">{title}</div>
