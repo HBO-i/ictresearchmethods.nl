009e0b20d6b39219d51a8b91a05053d9d9b13cdb
Coen Warries <99080092+NoWarries@users.noreply.github.com>
2024-01-31 17:09:03 +0100

    Fix ThemeSwitch.spec.ts to use themeState

--- tests/components/ThemeSwitch.spec.ts
+++ tests/components/ThemeSwitch.spec.ts
@@ -1,14 +1,15 @@
 import '@testing-library/jest-dom';
 
 import { render, fireEvent, screen } from '@testing-library/svelte';
-import { isJavaScriptDisabled, isDarkMode } from '$lib/stores';
+import { isJavaScriptDisabled } from '$lib/stores';
+import { themeState } from '$lib/stores/themeStore';
 
 import Comp from '$lib/components/ThemeSwitch.svelte';
 
 describe('ThemeSwitch', () => {
 	test("adds a 'dark' class to button if button is clicked", async () => {
 		isJavaScriptDisabled.set(false);
-		isDarkMode.set(false);
+		themeState.set('light');
 		render(Comp);
 
 		const switchButton = screen.getByTestId('themeToggle');
@@ -19,7 +20,7 @@ describe('ThemeSwitch', () => {
 	});
 
 	test("adds a 'dark' class to html if dark mode is enabled", () => {
-		isDarkMode.set(true);
+		themeState.set('dark');
 		const html = document.documentElement;
 
 		expect(html).toHaveClass('dark');
