d57613d9d37bffcada326247a31f09913a8188cf
Jochem Vogel <hello@jochemvogel.com>
2024-04-28 21:38:34 +0200

    Update research pattern navigator

---
+++ src/lib/components/QuestionCard.svelte
@@ -0,0 +1,130 @@
+<script lang="ts">
+	export let questionTitle: string;
+	export let questionPattern: string;
+
+	import { capitalizeFirstLetter, slugify } from '$lib/utils/format';
+	import Tag from './utils/Tag.svelte';
+</script>
+
+<a href={`/patterns/${slugify(questionPattern)}`} class="article-wrapper">
+	<article>
+		<div class="img-title">
+			<picture class="img">
+				<source type="image/webp" srcset={`/img/patterns/${slugify(questionPattern)}.webp`} />
+				<source type="image/png" srcset={`/img/patterns/${slugify(questionPattern)}.png`} />
+				<img
+					src={`/img/patterns/${slugify(questionPattern)}.png`}
+					loading="lazy"
+					decoding="async"
+					class="img"
+					alt=""
+					height="400"
+					width="400"
+				/>
+			</picture>
+			<h1>{questionTitle}</h1>
+		</div>
+		<p>{questionPattern}</p>
+		<!-- <picture class="img">
+			<source type="image/webp" srcset={`/img/patterns/${slugify(patternTitle)}.webp`} />
+			<source type="image/png" srcset={`/img/patterns/${slugify(patternTitle)}.png`} />
+			<img
+				src={`/img/patterns/${slugify(patternTitle)}.png`}
+				loading="lazy"
+				decoding="async"
+				class="img"
+				alt=""
+				height="400"
+				width="400"
+			/>
+		</picture>
+		<div class="heading-container">
+			<h1>{patternTitle}</h1>
+			<div class="tag-container">
+				{#each patternCategories as category}
+					<a href={`/${category}/`}>
+						<Tag theme={category} value={capitalizeFirstLetter(category)} />
+					</a>
+				{/each}
+			</div>
+		</div>
+		<div class="questions-container">
+			<h2>Questions:</h2>
+			<ul>
+				{#each patternQuestions as question}
+					<li>{question}</li>
+				{/each}
+			</ul>
+		</div> -->
+	</article>
+</a>
+
+<style lang="scss">
+	a {
+		text-decoration: none;
+
+		&:hover {
+			cursor: pointer;
+		}
+	}
+
+	article {
+		box-sizing: border-box;
+		background-color: var(--color-white);
+		border-radius: 0.5rem;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		height: 100%;
+		justify-content: space-between;
+		width: 100%;
+		padding: 1.5rem;
+		height: 100%;
+
+		&:hover {
+			transform: scale(1.01);
+			transition: transform 0.2s ease;
+		}
+
+		p {
+			font-size: 1.2em;
+			margin: 0;
+
+			&:hover {
+				text-decoration: underline;
+				cursor: pointer;
+			}
+
+			&::after {
+				content: ' ->';
+			}
+		}
+	}
+
+	.img {
+		height: 7.5rem;
+		border-radius: 10px;
+		width: 100%;
+		object-fit: contain;
+		background-color: var(--color-bg);
+
+		@include tablet {
+			height: 10rem;
+		}
+	}
+
+	h1 {
+		margin: 0;
+		font-size: 1.25em;
+
+		@include tablet-landscape {
+			font-size: 1.4em;
+		}
+	}
+
+	.img-title {
+		display: flex;
+		flex-direction: column;
+		gap: 8px;
+	}
+</style>
--- src/routes/research-pattern-navigator/+page.svelte
+++ src/routes/research-pattern-navigator/+page.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
 	import PatternCard from '$lib/components/PatternCard.svelte';
+	import QuestionCard from '$lib/components/QuestionCard.svelte';
 	import Alert from '$lib/components/utils/Alert.svelte';
+
+	const Pattern = {
+		CLARIFY_FOCUS_AND_SCOPE: 'Clarify focus and scope',
+		SOLIDIFY: 'Solidify',
+		VALIDATE: 'Validate',
+		CHOOSE_FITTING_TECHNOLOGY: 'Choose fitting technology',
+		REALISE_AS_REQUIRED: 'Realise as required',
+		REALISE_AS_AN_EXPERT: 'Realise as an expert'
+	};
 
 	// @TODO: Find cleaner way of storing this (+page.ts?)
 	const patternsArray = [
@@ -46,6 +56,58 @@
 			]
 		}
 	];
+
+	const questionsArray = [
+		{
+			question: 'How do I get a grasp on what the stakeholders really want?',
+			categories: ['field', 'workshop'],
+			pattern: Pattern.CLARIFY_FOCUS_AND_SCOPE
+		},
+		{
+			question: 'How can I help the stakeholders get clear what they really want?',
+			categories: ['field', 'workshop'],
+			pattern: Pattern.CLARIFY_FOCUS_AND_SCOPE
+		},
+		{
+			question: 'How do I develop a deep enough understanding of the context using available work?',
+			categories: ['field', 'library'],
+			pattern: Pattern.SOLIDIFY
+		},
+		{
+			question: 'How do I monitor the quality of my solution?',
+			categories: ['workshop', 'lab'],
+			pattern: Pattern.VALIDATE
+		},
+		{
+			question: 'How do I choose the right technologies for my project?',
+			categories: ['library', 'field', 'workshop'],
+			pattern: Pattern.CHOOSE_FITTING_TECHNOLOGY
+		},
+		{
+			question:
+				'How can I ensure the product meets the needs of the stakeholders throughout the project?',
+			categories: ['field', 'workshop', 'lab'],
+			pattern: Pattern.REALISE_AS_REQUIRED
+		},
+		{
+			question:
+				'How do I develop a solution that is more generally applicable than just for my immediate stakeholders?',
+			categories: ['library', 'workshop'],
+			pattern: Pattern.REALISE_AS_AN_EXPERT
+		},
+		{
+			question:
+				'How do I develop a state-of-the-art solution that meets professional requirements?',
+			categories: ['library', 'workshop'],
+			pattern: Pattern.REALISE_AS_AN_EXPERT
+		},
+		{
+			question:
+				'How do I develop something using technologies or techniques that I don’t know yet?',
+			categories: ['library', 'workshop'],
+			pattern: Pattern.REALISE_AS_AN_EXPERT
+		}
+	];
 </script>
 
 <svelte:head>
@@ -67,13 +129,9 @@
 
 <section>
 	<ul>
-		{#each patternsArray as pattern}
+		{#each questionsArray as question}
 			<li>
-				<PatternCard
-					patternTitle={pattern.title}
-					patternCategories={pattern.categories}
-					patternQuestions={pattern.questions}
-				/>
+				<QuestionCard questionTitle={question.question} questionPattern={question.pattern} />
 			</li>
 		{/each}
 	</ul>
@@ -83,13 +141,14 @@
 	ul {
 		list-style: none;
 		padding-left: 0;
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+		gap: 1.5rem;
 	}
 
-	li {
-		margin: 2rem 0;
-	}
-
-	p {
-		max-width: 90vw;
+	@media (max-width: 768px) {
+		ul {
+			grid-template-columns: 1fr;
+		}
 	}
 </style>
